e3b6ce0b44e4d1a9488c4b9fbc7dd09cbda46a73
GaboAlucema <114631424+GaboAlucema@users.noreply.github.com>
2024-06-23 22:58:23 -0400

    actualización n^3 fun palindromos

--- A_Algoritmo.py
+++ A_Algoritmo.py
@@ -7,31 +7,28 @@ inicio = time.time()
 #Funcion que recibe una cadena de texto y devuelve el numero minimo de cortes que se deben hacer para que cada subcadena sea un palindromo
 def palindromos (cadena):
     n = len(cadena)
-
-    C = [[0 for _ in range(n)] for _ in range(n)]                  #Matriz de cortes
-    P = [[False for _ in range(n)] for _ in range(n)]        #Matriz de palindromos
-
+    #Matriz de cortes
+    C = [[0 for _ in range(n)] for _ in range(n)]  
+    #Matriz de palindromos
+    P = [[False for _ in range(n)] for _ in range(n)]        
     for i in range(n):
-        P[i][i] = True                                       
+        P[i][i] = True
+        C[i][i] = 0                                     
 
-    for longitud in range(2, n+1):                          
+    for longitud in range(2, n + 1):                          
         for i in range(n - longitud + 1):                       
             j = i + longitud - 1                                
-
             if longitud == 2:                           
                 P[i][j] = (cadena[i] == cadena[j])       
             else:
-                P[i][j] = (cadena[i] == cadena[j]) and P[i + 1][j - 1]    
-
-    for i in range(n):
-        if P[0][i]:      
-            C[0][i] = 0
-        else:
-            C[0][i] = float('inf')     
-            for j in range(i):
-                if P[j + 1][i] and 1 + C[0][j] < C[0][i]:      
-                    C[0][i] = 1 + C[0][j]
-    return C[0][n-1]
+                P[i][j] = (cadena[i] == cadena[j]) and P[i + 1][j - 1] 
+            if P[i][j]:
+                C[i][j] = 0
+            else:
+                C[i][j] = float('inf')     
+                for k in range(i, j):
+                    C[i][j] = min(C[i][j], 1 + C[i][k] + C[k + 1][j])
+    return C[0][n - 1]
 
 #Funcion para leer archivo
 def leerarchivo(nombre):
