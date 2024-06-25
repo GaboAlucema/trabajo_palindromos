5b193fcedb846e6dc727fabb687a1503f56f02bf
GaboAlucema <114631424+GaboAlucema@users.noreply.github.com>
2024-06-24 23:04:47 -0400

    actualizacion programa

--- B_Algoritmo.py
+++ B_Algoritmo.py
@@ -4,29 +4,28 @@ Descripcion: Programa que recibe una cadena de texto y devuelve el numero minimo
 import time
 inicio = time.time()
 
-def minCutPalindrome(s):
+def minCortePalindromo(s):
     n = len(s)
     # Crear una matriz para almacenar los resultados de los subproblemas
-    minCutDp = [float('inf')] * n
-    # Crear una matriz para almacenar si una subcadena es palindrómica o no
-    isPalindrome = [[False] * n for _ in range(n)]
+    minCorteDp = [float('inf')] * n
+    # Crear una matriz para almacenar si una subcadena es palindromica o no
+    esPalindromo = [[False] * n for _ in range(n)]
 
     for i in range(n):
-        minCutDp[i] = i  # En el peor caso, se necesitan i cortes para una cadena de longitud i+1
-        isPalindrome[i][i] = True  # Una sola letra siempre es palindrómica
-
+        minCorteDp[i] = i
+        esPalindromo[i][i] = True  
     for i in range(1, n):
         for j in range(i + 1):
-            # Comprobar si la subcadena s[j:i+1] es palindrómica
-            if s[j] == s[i] and (i - j <= 1 or isPalindrome[j + 1][i - 1]):
-                isPalindrome[j][i] = True
-                # Actualizar el número mínimo de cortes necesarios
+            # Comprobar si la subcadena s[j:i+1] es palindromica
+            if s[j] == s[i] and (i - j <= 1 or esPalindromo[j + 1][i - 1]):
+                esPalindromo[j][i] = True
+                # Actualizar el numero minimo de cortes necesarios
                 if j == 0:
-                    minCutDp[i] = 0
+                    minCorteDp[i] = 0
                 else:
-                    minCutDp[i] = min(minCutDp[i], minCutDp[j - 1] + 1)
+                    minCorteDp[i] = min(minCorteDp[i], minCorteDp[j - 1] + 1)
 
-    return minCutDp[n - 1]
+    return minCorteDp[n - 1]
 
 #Funcion para leer archivo
 def leerarchivo(nombre):
@@ -44,7 +43,7 @@ nombre_archivo = "12000palabras.txt"
 ejemplos = leerarchivo(nombre_archivo)
 
 for cadena in ejemplos:
-    resultado = minCutPalindrome(cadena)
+    resultado = minCortePalindromo(cadena)
     print(f"Cadena: '{cadena}', Resultado: {resultado}")
 
 final = time.time()
